28697f0d4494772239b4f0291d980da75f71b62c
Jaspinder Kaur <101867787+Jaspinder-kaur@users.noreply.github.com>
2023-01-03 04:06:07 +0530

    Create Add_Two_Numbers.cpp

---
+++ Milestone 1/Lecture 1/Add_Two_Numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int first_no, second_no;
+    cout << "Number 1 : " << endl;
+    cin >> first_no;
+     cout << "Number 2 : " << endl;
+    cin >> second_no;
+    int sum = 0;
+    sum = first_no + second_no;
+    cout<<"Sum of Two Numbers is : " << sum;
+
+    return 0;
+}
