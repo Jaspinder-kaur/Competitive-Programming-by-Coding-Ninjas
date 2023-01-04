58b39f42e8d3cacf5b4a589359721005cf2786b6
Jaspinder Kaur <101867787+Jaspinder-kaur@users.noreply.github.com>
2023-01-04 15:10:44 +0530

    Create Even_or_Odd.cpp

---
+++ Milestone 1/Lecture 1/Even_or_Odd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "Enter Number : ";
+    cin >> num;
+    if(num%2==0)
+    {
+       cout<<"Even"; 
+    }
+    else 
+    {
+        cout<<"Odd";
+    }
+    return 0;
+}
