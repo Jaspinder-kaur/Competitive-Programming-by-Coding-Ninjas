42c725fb1e210ddb0b1a885b845082e60181c8ba
Jaspinder Kaur <101867787+Jaspinder-kaur@users.noreply.github.com>
2023-01-16 10:04:57 +0530

    Create Print_till_N.cpp

---
+++ Milestone 1/Lecture_3/Print_till_N.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+int main()
+{
+    int num,count=1;
+    cout << "Enter Number" << endl;
+    cin >> num;
+        while(count <= num)
+        {
+          cout << count << endl;
+        count = count + 1;  
+        }
+}
