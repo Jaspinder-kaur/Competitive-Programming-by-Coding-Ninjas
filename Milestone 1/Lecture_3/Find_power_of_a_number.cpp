7bb54d21c28857dedc36d473e3de999123e33973
Jaspinder Kaur <101867787+Jaspinder-kaur@users.noreply.github.com>
2023-01-20 12:52:09 +0530

    Create Find_power_of_a_number.cpp

---
+++ Milestone 1/Lecture_3/Find_power_of_a_number.cpp
@@ -0,0 +1,35 @@
+/*
+Find power of a number
+Send Feedback
+Write a program to find x to the power n (i.e. x^n). Take x and n from the user. You need to print the answer.
+Note : For this question, you can assume that 0 raised to the power of 0 is 1
+
+Input format :
+Two integers x and n (separated by space)
+Output Format :
+x^n (i.e. x raise to the power n)
+Constraints:
+0 <= x <= 8 
+0 <= n <= 9
+Sample Input 1 :
+ 3 4
+Sample Output 1 :
+81
+*/
+
+#include<iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+int x, n,result=1;
+cin >> x >> n;
+if(x>=0 || x <= 8)
+{
+	if(n >=0 || n <= 9)
+	{
+		result = pow(x,n);
+		cout << result;
+	}
+}
+}
