88771c124aaac6cabd6bec03056ba55006d661be
Jaspinder Kaur <101867787+Jaspinder-kaur@users.noreply.github.com>
2023-01-26 00:27:01 +0530

    Update Sum_of_Even_till_N.cpp

--- Milestone 1/Lecture_3/Sum_of_Even_till_N.cpp
+++ Milestone 1/Lecture_3/Sum_of_Even_till_N.cpp
@@ -1,18 +1,19 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-		int num, sum=0;
+int main()
+{
+   int num, sum=0;
        cin >> num;
 
-	   int i = 1;
-	   while(i <= num)
-{
-	if(i%2==0)
-	{
-		sum += i;
-	}
+   int i = 1;
+	 while(i <= num)
+	  {
+	    if(i%2==0)
+	      {
+		 sum += i;
+	      }
 	i++;
-}
-cout << sum << endl; 
+	 }
+   cout << sum << endl; 
 }
