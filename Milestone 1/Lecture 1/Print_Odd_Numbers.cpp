0b862539c4314705e4e31a08427cd4b09e708377
Jaspinder Kaur <101867787+Jaspinder-kaur@users.noreply.github.com>
2023-01-28 20:43:39 +0530

    Update Print_Odd_Numbers.cpp

--- Milestone 1/Lecture 1/Print_Odd_Numbers.cpp
+++ Milestone 1/Lecture 1/Print_Odd_Numbers.cpp
@@ -7,7 +7,7 @@ int main()
     int num, next = 1;
     cout << "Enter Number :" << endl;
     cin >> num;
-    cout << "All even between 1 To N : " << endl;
+    cout << "All odd numbers between 1 To N : " << endl;
     while(next <= num)
     {
       if(next % 2 == 1)
